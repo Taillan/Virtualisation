47e709ba853f2a6fbf2fd45cbcce0fcad8a3db92
vincent monnot <vincent.monnot@edu.esiee.fr>
2023-03-24 13:27:40 +0100

    mysql fix

--- src/quiz-api/utils/db_connect.py
+++ src/quiz-api/utils/db_connect.py
@@ -1,31 +1,30 @@
 import sqlite3
 from flask import g
-from mysql.connector import (connection)
-
-database = "db/database.db"
-
+import mysql.connector
+import os
 
 def get_db():
     con = getattr(g, '_database', None)
     if con is None:
-        con = connection.MySQLConnection(
-            host='127.0.0.1',
-            user='root',
-            password='rootroot',
-            database='QuizzDB')
+        con = g._database = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DB")
+            )
     return con
 
 def get_cur():
     cur = getattr(g, '_cursor', None)
     if cur is None:
-        cur = g._cursor = get_db().cursor()
+        cur = g._cursor = get_db().cursor(buffered=True)
     return cur
 
 def db_connection(instruction):
     try:
         #création d'un objet connection
-        sql_result = get_cur().execute(instruction)            
-        get_db().commit()        
-        return sql_result
-    except sqlite3.Error as er:
+        get_cur().execute(instruction)
+        get_db().commit()
+        return get_cur()
+    except ValueError as er:
         print("error : ", er)
